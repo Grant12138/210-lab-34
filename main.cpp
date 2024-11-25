3fc0d5582da2d5ee2fd60f401005006d9b45ce46
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-11-24 18:29:14 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -3,3 +3,51 @@
 * Editor: CLion
 * Compiler: Apple clang version 16.0.0
 */
+#include <iosteam>
+using namespace std;
+
+struct Edge {
+    int src, dest, weight;
+};
+
+typedef pair<int, int> Pair;  // Creates an alias 'Pair' for the pair<int,int> data type
+
+class Graph {
+    public:
+        // a vector of vectors of Pairs to represent an adjacency list
+        vector<vector<Pair>> adjList;
+        // constructor
+        Graph(vector<Edge> const &edges) {
+            // resize the vector to hold SIZE elements of type vector<Edge>
+            adjList.resize(SIZE);
+            // add edges to the directed graph
+            for (auto &edge: edges) {
+                int src = edge.src;
+                int dest = edge.dest;
+                int weight = edge.weight;
+                adjList[src].push_back(make_pair(dest, weight));
+                adjList[dest].push_back(make_pair(src, weight));
+            }
+        }
+        void printGraph() {
+            for (int i = 0; i < adjList.size(); i++) {
+                for (Pair v: adjList[i])
+                    cout << "(" << i << ", " << v.first << ", " << v.second << ") ";
+                cout << endl;
+            }
+        }
+}
+
+vector<Edge> edges = {
+        // (x, y, w) —> edge from x to y having weight w
+        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+};
+
+int main()
+{
+    // Creates graph
+    Graph graph(edges);
+
+    return 0;
+}
+
